90d2daedf049c75e1d413998fb2b4d2c74eff5c3
Gabriel <75986085+xGabrielR@users.noreply.github.com>
2021-11-21 23:01:20 -0300

    Update Atividades01.sql

--- Atividades/SQL & SQLite Fundamentos/Atividades01.sql
+++ Atividades/SQL & SQLite Fundamentos/Atividades01.sql
@@ -2,9 +2,27 @@
 ----------- Base de Dados 'PrimeiraBaseDeDados' --------------
 
 /*
-    1. Qual a primeira  pessoa que aparece com o genero masculino, em uma ordenação decresçente?
-    2. Qual a primeira  pessoa que aparece com o genero feminino, em uma ordenação decresçente?
-    3. Qual é o total de pessoas que estão registradas na base de dados?
-    4. Quantas pessoas nasceram antes do ano 2000?
-    5. Atualize o nome de 'José' para 'Carlos'.
+    1. Crie um Diagrama no Paint mesmo contendo apenas a Tabela Carro e as seguintes colunas:
+        - Placa           Texto Chave Primária Não Nulo Única.
+        - Modelo          Texto Não Nulo.
+        - Classificacao   Texto Não Nulo.
+        - Cor             Texto Não Nulo.
+        - Preco           Inteiro Não Nulo.
+    2. Insira dados na tabela que você acabou de criar, use a internet para pegar as devidas informações.
+        2.1. Requisitos: 
+            - Deve ter pelo menos dois veiculos com a cor igual a "Verde".
+            - Deve ter preços coerentes para responder o exercicio 3.
+            - Deve ter pelo menos 5 registros na base de dados.
+    
+    3. Realize um Relatório respondendo as seguintes perguntas: 
+        3.1. Qual é a primeira Classificação de veiculos que aparece com a cor verde, em uma ordenação decresçente?
+        3.2. Qual é a primeiro Modelo que aparece com o Preço , em uma ordenação decresçente?
+        3.3. Qual é o total de carros que estão registradas na base de dados?
+        3.4. Qual é a placa dos automóveis dos seguintes Id (1, 3, 5) ?
+        
+    4. Atualize algum registro de veiculo atualizando sua Placa para "MNB-5050".
+    5. Atualize a cor do veículo com a placa "MNB-5050" para "Azul".
+    6. Atualize a classificação do veiculo com a placa "MNB-5050" para o tipo "caminhao-trator"
+        
+        -- Obrigado por realizar os exercicios, qualquer dívida basta consultar as respostas.
 */
